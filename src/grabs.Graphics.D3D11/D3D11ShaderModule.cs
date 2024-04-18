408046a532203be8a17ab9a3814a67347ad0b0bd
ollie <ollie@ollierobinson.co.uk>
2024-04-18 16:44:08 +0100

    split dx shader compiler to Spirv project

--- src/grabs.Graphics.D3D11/D3D11ShaderModule.cs
+++ src/grabs.Graphics.D3D11/D3D11ShaderModule.cs
@@ -1,76 +1,19 @@
 ﻿using System;
-using Silk.NET.SPIRV;
+using grabs.ShaderCompiler.Spirv;
 using Silk.NET.SPIRV.Cross;
 using Vortice.D3DCompiler;
 using Vortice.Direct3D;
 using Compiler = Vortice.D3DCompiler.Compiler;
-using SpvCompiler = Silk.NET.SPIRV.Cross.Compiler;
 
 namespace grabs.Graphics.D3D11;
 
 public sealed class D3D11ShaderModule : ShaderModule
 {
-    public static Cross Spirv;
-
-    static D3D11ShaderModule()
-    {
-        Spirv = Cross.GetApi();
-    }
-
-    public static unsafe string SpirVToHLSL(ShaderStage stage, byte[] spirv, string entryPoint)
-    {
-        Result result;
-        
-        Context* context;
-        if ((result = Spirv.ContextCreate(&context)) != Result.Success)
-            throw new Exception($"Failed to create context: {result} - {Spirv.ContextGetLastErrorStringS(context)}");
-
-        ParsedIr* ir;
-        fixed (byte* pSpirv = spirv)
-        {
-            if ((result = Spirv.ContextParseSpirv(context, (uint*) pSpirv, (nuint) (spirv.Length / sizeof(uint)), &ir)) != Result.Success)
-                throw new Exception($"Failed to parse SPIRV: {result} - {Spirv.ContextGetLastErrorStringS(context)}");
-        }
-
-        SpvCompiler* compiler;
-        if ((result = Spirv.ContextCreateCompiler(context, Backend.Hlsl, ir, CaptureMode.TakeOwnership, &compiler)) != Result.Success)
-            throw new Exception($"Failed to create compiler: {result} - {Spirv.ContextGetLastErrorStringS(context)}");
-
-        CompilerOptions* options;
-        if ((result = Spirv.CompilerCreateCompilerOptions(compiler, &options)) != Result.Success)
-            throw new Exception($"Failed to create compiler options: {result} - {Spirv.ContextGetLastErrorStringS(context)}");
-
-        ExecutionModel model = stage switch
-        {
-            ShaderStage.Vertex => ExecutionModel.Vertex,
-            ShaderStage.Pixel => ExecutionModel.Fragment,
-            ShaderStage.Compute => ExecutionModel.GLCompute, // GL compute?
-            _ => throw new ArgumentOutOfRangeException(nameof(stage), stage, null)
-        };
-
-        Spirv.CompilerSetEntryPoint(compiler, entryPoint, model);
-
-        // Use shader model 5.0
-        Spirv.CompilerOptionsSetUint(options, CompilerOption.HlslShaderModel, 50);
-        Spirv.CompilerInstallCompilerOptions(compiler, options);
-
-        byte* pStrResult;
-        if ((result = Spirv.CompilerCompile(compiler, &pStrResult)) != Result.Success)
-            throw new Exception($"Failed to compile SPIRV shader: {result} - {Spirv.ContextGetLastErrorStringS(context)}");
-
-        string strResult = new string((sbyte*) pStrResult);
-        
-        Spirv.ContextReleaseAllocations(context);
-        Spirv.ContextDestroy(context);
-
-        return strResult;
-    }
-    
     public Blob Blob;
     
     public D3D11ShaderModule(ShaderStage stage, byte[] spirv, string entryPoint) : base(stage)
     {
-        string hlsl = SpirVToHLSL(stage, spirv, entryPoint);
+        string hlsl = SpirvCompiler.TranspileSpirv(stage, Backend.Hlsl, spirv, entryPoint);
 
         string profile = stage switch
         {
---
+++ src/grabs.ShaderCompiler.Spirv/SpirvCompiler.cs
@@ -0,0 +1,64 @@
+﻿using grabs.Graphics;
+using Silk.NET.SPIRV;
+using Silk.NET.SPIRV.Cross;
+
+namespace grabs.ShaderCompiler.Spirv;
+
+public class SpirvCompiler
+{
+    public static Cross Spirv;
+
+    static SpirvCompiler()
+    {
+        Spirv = Cross.GetApi();
+    }
+    
+    public static unsafe string TranspileSpirv(ShaderStage stage, Backend backend, byte[] spirv, string entryPoint)
+    {
+        Result result;
+        
+        Context* context;
+        if ((result = Spirv.ContextCreate(&context)) != Result.Success)
+            throw new Exception($"Failed to create context: {result} - {Spirv.ContextGetLastErrorStringS(context)}");
+
+        ParsedIr* ir;
+        fixed (byte* pSpirv = spirv)
+        {
+            if ((result = Spirv.ContextParseSpirv(context, (uint*) pSpirv, (nuint) (spirv.Length / sizeof(uint)), &ir)) != Result.Success)
+                throw new Exception($"Failed to parse SPIRV: {result} - {Spirv.ContextGetLastErrorStringS(context)}");
+        }
+
+        Compiler* compiler;
+        if ((result = Spirv.ContextCreateCompiler(context, backend, ir, CaptureMode.TakeOwnership, &compiler)) != Result.Success)
+            throw new Exception($"Failed to create compiler: {result} - {Spirv.ContextGetLastErrorStringS(context)}");
+
+        CompilerOptions* options;
+        if ((result = Spirv.CompilerCreateCompilerOptions(compiler, &options)) != Result.Success)
+            throw new Exception($"Failed to create compiler options: {result} - {Spirv.ContextGetLastErrorStringS(context)}");
+
+        ExecutionModel model = stage switch
+        {
+            ShaderStage.Vertex => ExecutionModel.Vertex,
+            ShaderStage.Pixel => ExecutionModel.Fragment,
+            ShaderStage.Compute => ExecutionModel.GLCompute, // GL compute?
+            _ => throw new ArgumentOutOfRangeException(nameof(stage), stage, null)
+        };
+
+        Spirv.CompilerSetEntryPoint(compiler, entryPoint, model);
+
+        // Use shader model 5.0
+        Spirv.CompilerOptionsSetUint(options, CompilerOption.HlslShaderModel, 50);
+        Spirv.CompilerInstallCompilerOptions(compiler, options);
+
+        byte* pStrResult;
+        if ((result = Spirv.CompilerCompile(compiler, &pStrResult)) != Result.Success)
+            throw new Exception($"Failed to compile SPIRV shader: {result} - {Spirv.ContextGetLastErrorStringS(context)}");
+
+        string strResult = new string((sbyte*) pStrResult);
+        
+        Spirv.ContextReleaseAllocations(context);
+        Spirv.ContextDestroy(context);
+
+        return strResult;
+    }
+}
