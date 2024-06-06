addc2ed7e0d1bbacff8e74ffee0090ec3a21a5b3
ollie <ollie@ollierobinson.co.uk>
2024-06-06 18:55:32 +0100

    begin grabs.Audio

---
+++ src/grabs.Audio/AudioBuffer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace grabs.Audio;
+
+public struct AudioBuffer : IDisposable
+{
+    private ulong _id;
+    private Context _context;
+    
+    public void Dispose()
+    {
+        
+    }
+}
---
+++ src/grabs.Audio/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace grabs.Audio;
+
+public sealed class Context : IDisposable
+{
+    
+    
+    public void Dispose()
+    {
+        
+    }
+}
