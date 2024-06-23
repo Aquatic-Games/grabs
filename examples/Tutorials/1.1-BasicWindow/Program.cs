49f25ba7fe5366c18ed5723333574cae1abec0b2
ollie <ollie@ollierobinson.co.uk>
2024-06-23 19:12:19 +0100

    started tutorials

---
+++ examples/Tutorials/1.1-BasicWindow/Program.cs
@@ -0,0 +1,10 @@
+﻿using grabs.Graphics;
+using grabs.Windowing;
+
+WindowInfo windowInfo = new WindowInfo("Tutorial 1.1 - Basic Window", 1280, 720);
+using Window window = new Window(windowInfo);
+
+using Instance instance = window.CreateInstance();
+using Surface surface = window.CreateSurface();
+
+using Device device = instance.CreateDevice(surface);
