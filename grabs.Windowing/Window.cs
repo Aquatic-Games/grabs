d57f216988aab438d71fbba382297ead0978a37e
ollie <ollie@ollierobinson.co.uk>
2024-06-23 15:08:13 +0100

    start grabs.Windowing

---
+++ grabs.Windowing/Window.cs
@@ -0,0 +1,14 @@
+﻿namespace grabs.Windowing;
+
+public sealed class Window : IDisposable
+{
+    public Window(WindowInfo info)
+    {
+        
+    }
+    
+    public void Dispose()
+    {
+        
+    }
+}
---
+++ grabs.Windowing/WindowInfo.cs
@@ -0,0 +1,12 @@
+namespace grabs.Windowing;
+
+public struct WindowInfo
+{
+    public string Title;
+
+    public uint Width;
+    public uint Height;
+
+    public int? X;
+    public int? Y;
+}
