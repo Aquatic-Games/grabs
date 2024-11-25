650baa68c97eb3c0d4ab12ced1a6b3da2bd63673
Aqua <ollie@ollierobinson.co.uk>
2024-11-25 16:50:36 +0000

    start shader modules

--- include/grabs/Device.h
+++ include/grabs/Device.h
@@ -7,6 +7,7 @@
 #include "Swapchain.h"
 #include "CommandList.h"
 #include "Buffer.h"
+#include "ShaderModule.h"
 
 namespace grabs
 {
@@ -18,6 +19,7 @@ namespace grabs
         virtual std::unique_ptr<Swapchain> CreateSwapchain(const SwapchainDescription& description, Surface* surface) = 0;
         virtual std::unique_ptr<CommandList> CreateCommandList() = 0;
         virtual std::unique_ptr<Buffer> CreateBuffer(const BufferDescription& description, void* data) = 0;
+        virtual std::unique_ptr<ShaderModule> CreateShaderModule(const ShaderModuleDescription& description) = 0;
 
         virtual void SubmitCommandList(CommandList* list) = 0;
     };
---
+++ include/grabs/ShaderModule.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <string>
+
+namespace grabs
+{
+    enum class ShaderStage
+    {
+        Vertex,
+        Pixel,
+        Geometry,
+        Compute
+    };
+
+    struct ShaderModuleDescription
+    {
+        ShaderStage Stage;
+        uint8_t* Spirv;
+        std::string EntryPoint;
+    };
+
+    class ShaderModule
+    {
+    public:
+        virtual ~ShaderModule() = default;
+    };
+}
