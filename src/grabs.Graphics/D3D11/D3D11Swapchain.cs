6e4a79424b2720f8c499071da8ff534a524c2770
Aqua <ollie@ollierobinson.co.uk>
2025-02-15 13:51:49 +0000

    start d3d11 swapchain

---
+++ src/grabs.Graphics/D3D11/D3D11Swapchain.cs
@@ -0,0 +1,42 @@
+﻿using Vortice.Direct3D11;
+using Vortice.DXGI;
+
+namespace grabs.Graphics.D3D11;
+
+internal sealed class D3D11Swapchain : Swapchain
+{
+    public readonly IDXGISwapChain Swapchain;
+    
+    public D3D11Swapchain(IDXGIFactory factory, ID3D11Device device, D3D11Surface surface, ref readonly SwapchainInfo info)
+    {
+        SwapChainDescription swapchainDesc = new SwapChainDescription()
+        {
+            OutputWindow = surface.Hwnd,
+            Windowed = true,
+
+            BufferCount = info.NumBuffers,
+            BufferDescription = new ModeDescription(info.Size.Width, info.Size.Height, info.Format.ToD3D()),
+            BufferUsage = Usage.RenderTargetOutput,
+            
+            SampleDescription = new SampleDescription(1, 0),
+            SwapEffect = SwapEffect.FlipDiscard
+        };
+
+        Swapchain = factory.CreateSwapChain(device, swapchainDesc);
+    }
+    
+    public override Texture GetNextTexture()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public override void Present()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public override void Dispose()
+    {
+        Swapchain.Dispose();
+    }
+}
