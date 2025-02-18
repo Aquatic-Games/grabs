76ab1edd87fbf9cccabdf305cad9a9ad0529a76e
Aqua <ollie@ollierobinson.co.uk>
2025-02-18 22:29:02 +0000

    start d3d shader modules

---
+++ src/grabs.Graphics/D3D11/D3D11ShaderModule.cs
@@ -0,0 +1,18 @@
+﻿using Vortice.Direct3D;
+
+namespace grabs.Graphics.D3D11;
+
+internal sealed class D3D11ShaderModule : ShaderModule
+{
+    public readonly Blob Blob;
+
+    public D3D11ShaderModule(ShaderStage stage, ReadOnlySpan<byte> spirv, string entryPoint)
+    {
+        
+    }
+    
+    public override void Dispose()
+    {
+        Blob.Dispose();
+    }
+}
---
+++ src/grabs.Graphics/Internal/CompilerUtils.cs
@@ -0,0 +1,18 @@
+﻿using Silk.NET.Shaderc;
+
+namespace grabs.Graphics.Internal;
+
+internal static class CompilerUtils
+{
+    private static readonly Shaderc _shaderc;
+
+    static CompilerUtils()
+    {
+        _shaderc = Shaderc.GetApi();
+    }
+    
+    public static string HlslFromSpirv(ShaderStage stage, in ReadOnlySpan<byte> spirv, string entryPoint)
+    {
+        return "";
+    }
+}
