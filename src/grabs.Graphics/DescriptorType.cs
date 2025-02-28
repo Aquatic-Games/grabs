067451c16375739f9ee75f66f3571953908525b5
Aqua <ollie@ollierobinson.co.uk>
2025-02-28 14:34:11 +0000

    start descriptors

---
+++ src/grabs.Graphics/DescriptorType.cs
@@ -0,0 +1,17 @@
+namespace grabs.Graphics;
+
+/// <summary>
+/// Defines various types a descriptor can be.
+/// </summary>
+public enum DescriptorType
+{
+    /// <summary>
+    /// A constant (uniform) buffer.
+    /// </summary>
+    ConstantBuffer,
+    
+    /// <summary>
+    /// A texture.
+    /// </summary>
+    Texture
+}
--- src/grabs.Graphics/ShaderStage.cs
+++ src/grabs.Graphics/ShaderStage.cs
@@ -3,8 +3,14 @@ namespace grabs.Graphics;
 /// <summary>
 /// Defines various stages a shader can be.
 /// </summary>
+[Flags]
 public enum ShaderStage
 {
+    /// <summary>
+    /// No shader stage. Do not use.
+    /// </summary>
+    None = 0,
+    
     /// <summary>
     /// A vertex shader.
     /// </summary>
@@ -13,5 +19,10 @@ public enum ShaderStage
     /// <summary>
     /// A pixel (fragment) shader.
     /// </summary>
-    Pixel
+    Pixel,
+    
+    /// <summary>
+    /// Vertex and pixel shader stages.
+    /// </summary>
+    VertexPixel = Vertex | Pixel
 }
