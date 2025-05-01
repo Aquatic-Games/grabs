6f2a20697a40f7b5e8a64d54c276dfeff0b98588
Aqua <ollie@ollierobinson.co.uk>
2025-05-02 00:31:42 +0100

    d3d11 swapchain texture

--- src/grabs.Graphics.D3D11/D3D11Swapchain.cs
+++ src/grabs.Graphics.D3D11/D3D11Swapchain.cs
@@ -4,6 +4,7 @@ using TerraFX.Interop.DirectX;
 using TerraFX.Interop.Windows;
 using static TerraFX.Interop.DirectX.DXGI_SWAP_EFFECT;
 using static TerraFX.Interop.DirectX.DXGI;
+using static TerraFX.Interop.Windows.Windows;
 
 namespace grabs.Graphics.D3D11;
 
@@ -16,8 +17,12 @@ internal sealed unsafe class D3D11Swapchain : Swapchain
 
     private readonly IDXGISwapChain* _swapchain;
 
+    private D3D11Texture _swapchainTexture;
+
     public D3D11Swapchain(IDXGIFactory1* factory, ID3D11Device* device, ref readonly SwapchainInfo info)
     {
+        BufferFormat = info.Format;
+        
         DXGI_SWAP_CHAIN_DESC swapchainDesc = new()
         {
             OutputWindow = (HWND) ((D3D11Surface) info.Surface).Hwnd,
@@ -30,25 +35,32 @@ internal sealed unsafe class D3D11Swapchain : Swapchain
                 Format = info.Format.ToD3D()
             },
             BufferCount = info.NumBuffers,
-            BufferUsage = DXGI_USAGE_BACK_BUFFER,
+            BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT,
 
-            SwapEffect = DXGI_SWAP_EFFECT_DISCARD,
+            SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD,
             SampleDesc = new DXGI_SAMPLE_DESC(1, 0),
         };
 
         GrabsLog.Log("Creating swapchain.");
         fixed (IDXGISwapChain** swapchain = &_swapchain)
             factory->CreateSwapChain((IUnknown*) device, &swapchainDesc, swapchain).Check("Create swapchain");
+
+        GrabsLog.Log("Getting swapchain texture.");
+        ID3D11Texture2D* swapchainTexture;
+        _swapchain->GetBuffer(0, __uuidof<ID3D11Texture2D>(), (void**) &swapchainTexture)
+            .Check("Get swapchain texture");
+
+        _swapchainTexture = new D3D11Texture(device, swapchainTexture, info.Size);
     }
     
     public override Texture GetNextTexture()
     {
-        return null;
+        return _swapchainTexture;
     }
     
     public override void Present()
     {
-        _swapchain->Present(1, 0);
+        _swapchain->Present(1, 0).Check("Present");
     }
     
     public override void Dispose()
@@ -57,6 +69,7 @@ internal sealed unsafe class D3D11Swapchain : Swapchain
             return;
         IsDisposed = true;
 
+        _swapchainTexture.Dispose();
         _swapchain->Release();
     }
 }
---
+++ src/grabs.Graphics.D3D11/D3D11Texture.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using grabs.Core;
+using TerraFX.Interop.DirectX;
+
+namespace grabs.Graphics.D3D11;
+
+[SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
+internal sealed unsafe class D3D11Texture : Texture
+{
+    public override bool IsDisposed { get; protected set; }
+
+    private readonly ID3D11Resource* _texture;
+
+    public readonly ID3D11RenderTargetView* RenderTarget;
+
+    public D3D11Texture(ID3D11Device* device, ID3D11Texture2D* texture, Size2D size) : base(size)
+    {
+        _texture = (ID3D11Resource*) texture;
+
+        GrabsLog.Log("Creating render target");
+        fixed (ID3D11RenderTargetView** renderTarget = &RenderTarget)
+            device->CreateRenderTargetView(_texture, null, renderTarget).Check("Create render target");
+    }
+    
+    public override void Dispose()
+    {
+        if (IsDisposed)
+            return;
+        IsDisposed = true;
+
+        if (RenderTarget != null)
+            RenderTarget->Release();
+
+        _texture->Release();
+    }
+}
